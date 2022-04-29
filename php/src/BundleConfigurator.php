9090353b61a6e32ad1e417ffca82a2bd5e906e1c
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-29 13:53:23 +0200

    fake-it

--- php/src/BundleConfigurator.php
+++ php/src/BundleConfigurator.php
@@ -7,45 +7,70 @@ final class BundleConfigurator
 {
     public function select(string $productNames): string
     {
-        if ($productNames === 'P1,P2,P5') {
-            return 'B5,P2';
-        }
-
-        if ($productNames === 'P1,P2,P3,P4') {
-            return 'B4';
-        }
-
         return $this->bundleDetector($productNames);
-
     }
 
     private function bundleDetector(string $productNames): string
     {
         $bundleConfiguration = $this->bundles();
         $arrayProductNames = explode(',', $productNames);
+        $possibleBundle = [];
 
         foreach ($bundleConfiguration as $bundle => $bundleProducts) {
 
-            if(!array_diff($bundleProducts, $arrayProductNames)) {
-                foreach ($bundleProducts as $product) {
-                    unset($arrayProductNames[array_search($product, $arrayProductNames)]);
-                }
-                array_unshift($arrayProductNames, $bundle);
+            if(!array_diff($bundleProducts['products'], $arrayProductNames)) {
+                $possibleBundle[] = $bundle;
             }
-            $productNames = implode(',', $arrayProductNames);
         }
 
-        return $productNames;
+        $maxPriceReduce = 0;
+        $bestBundle = '';
+        foreach ($possibleBundle as $bundle) {
+            if ($bundleConfiguration[$bundle]['price'] > $maxPriceReduce) {
+                $maxPriceReduce = $bundleConfiguration[$bundle]['price'];
+                $bestBundle = $bundle;
+            }
+        }
+
+        if (!empty($bestBundle)) {
+            $arrayProductNames = $this->getArrayProductNames($bundleConfiguration[$bestBundle]['products'], $arrayProductNames, $bestBundle);
+        }
+
+        return implode(',', $arrayProductNames);
     }
 
     private function bundles(): array
     {
         return [
-            'B1' => ['P1','P2'],
-            'B2' => ['P1','P4'],
-            'B3' => ['P3','P4'],
-            'B4' => ['P1','P2','P3','P4'],
-            'B5' => ['P1','P5'],
+            'B1' => [
+                'products' => ['P1','P2'],
+                'price'    => 25,
+            ],
+            'B2' => [
+                'products' => ['P1','P4'],
+                'price'    => 40,
+            ],
+            'B3' => [
+                'products' => ['P3','P4'],
+                'price'    => 60,
+            ],
+            'B4' => [
+                'products' => ['P1','P2','P3','P4'],
+                'price'    => 80,
+            ],
+            'B5' => [
+                'products' => ['P1','P5'],
+                'price'    => 50,
+            ],
         ];
+    }
+
+    private function getArrayProductNames($products, $arrayProductNames, $bundle): array
+    {
+        foreach ($products as $product) {
+            unset($arrayProductNames[array_search($product, $arrayProductNames)]);
+        }
+        array_unshift($arrayProductNames, $bundle);
+        return $arrayProductNames;
     }
 }
