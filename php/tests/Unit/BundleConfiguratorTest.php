77408e14f31333aacdfb93dfc6cf3417daaeb0c0
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-29 14:02:01 +0200

    refactor test

--- php/tests/Unit/BundleConfiguratorTest.php
+++ php/tests/Unit/BundleConfiguratorTest.php
@@ -8,71 +8,38 @@ use PHPUnit\Framework\TestCase;
 
 final class BundleConfiguratorTest extends TestCase
 {
-    public function individualProductsProvider(): array
-    {
-        return [
-            ['P1'],
-            ['P2'],
-            ['P3'],
-            ['P4'],
-            ['P5'],
-        ];
-    }
-
     /**
      * @test
-     * @dataProvider individualProductsProvider
+     * @dataProvider validOrderProductsProvider
      */
-    public function should_not_return_bundle_for_individual_product(string $productName): void
-    {
-        $bundleConfigurator = new BundleConfigurator();
-
-        self::assertSame($productName, $bundleConfigurator->select($productName));
-    }
-
-    public function twoOrMoreProductsProvider(): array
-    {
-        return [
-            ['P1,P2', 'B1'],
-            ['P1,P4', 'B2'],
-            ['P3,P4', 'B3'],
-            ['P1,P2,P3,P4', 'B4'],
-            ['P1,P5', 'B5'],
-        ];
-    }
-
-    /**
-     * @test
-     * @dataProvider twoOrMoreProductsProvider
-     */
-    public function should_create_bundle_for_two_or_more_product(string $productsName, string $expectBundle): void
+    public function should_create_bundle_with_valid_order(string $productsName, string $expectBundle): void
     {
         $bundleConfigurator = new BundleConfigurator();
 
         self::assertSame($expectBundle, $bundleConfigurator->select($productsName));
     }
 
-    /** @test */
-    public function should_create_bundle_and_return_leftover_products(): void
+    public function validOrderProductsProvider(): array
     {
-        $bundleConfigurator = new BundleConfigurator();
-
-        self::assertSame('B1,P1', $bundleConfigurator->select('P1,P2,P1'));
-    }
-
-    /** @test */
-    public function should_create_bundle_with_products_in_different_order(): void
-    {
-        $bundleConfigurator = new BundleConfigurator();
-
-        self::assertSame('B1', $bundleConfigurator->select('P2,P1'));
-    }
-
-    /** @test */
-    public function should_create_bundle_with_products_in_different_order_B2(): void
-    {
-        $bundleConfigurator = new BundleConfigurator();
-
-        self::assertSame('B2', $bundleConfigurator->select('P4,P1'));
+        return [
+            ['P1', 'P1'],
+            ['P2', 'P2'],
+            ['P3', 'P3'],
+            ['P4', 'P4'],
+            ['P5', 'P5'],
+            ['P1,P2', 'B1'],
+            ['P1,P4', 'B2'],
+            ['P3,P4', 'B3'],
+            ['P1,P2,P3,P4', 'B4'],
+            ['P1,P5', 'B5'],
+            ['P1,P2,P1', 'B1,P1'],
+            ['P2,P1', 'B1'],
+            ['P4,P1', 'B2'],
+            ['P1,P2,P3', 'B1,P3'],
+            ['P2,P4', 'P2,P4'],
+            ['P1,P3,P4', 'B3,P1'],
+            ['P2,P3,P4', 'B3,P2'],
+            ['P1,P2,P5', 'B5,P2'],
+        ];
     }
 }
